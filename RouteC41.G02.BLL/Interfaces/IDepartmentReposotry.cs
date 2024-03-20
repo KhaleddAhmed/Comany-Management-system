67f1d050dc7aafce526a790e4eebb338903e4863
Khale <Khale@LAPTOP-8TGIV0NP>
2024-03-20 23:08:26 +0200

    Layers Arch-BLL

---
+++ RouteC41.G02.BLL/Interfaces/IDepartmentReposotry.cs
@@ -0,0 +1,25 @@
+﻿using RouteC41.G02.DAL.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteC41.G02.BLL.Interfaces
+{
+	internal interface IDepartmentReposotry
+	{
+		IEnumerable<Department> GetAll();
+
+		Department GetById(int id);
+
+		int Add(Department entity);
+
+		int Update(Department entity);
+
+		int Delete(Department entity);
+
+
+	}
+}
