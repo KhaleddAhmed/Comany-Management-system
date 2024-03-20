11665572b99926ce0f005c1373fcc66ba8ce380c
Khale <Khale@LAPTOP-8TGIV0NP>
2024-03-20 16:58:18 +0200

    Layres Architecture Pattern -DAL for Department Module

---
+++ RouteC41.G02.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RouteC41.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteC41.G02.DAL.Data
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        
+          => optionsBuilder.UseSqlServer(".;Database = MVCApplicationG02;Trusted_Connection = True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        DbSet<Department> Departments { get; set; }
+    }
+}
