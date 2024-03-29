8907a6b2ab7d90159af4741b1160eef352ea6db8
Khale <Khale@LAPTOP-8TGIV0NP>
2024-03-29 08:29:48 +0200

    Employee Repository-BLL

---
+++ RouteC41.G02.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using RouteC41.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteC41.G02.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+
+        Employee GetById(int id);
+
+        int Add(Employee entity);
+
+        int Update(Employee entity);
+
+        int Delete(Employee entity);
+    }
+}
---
+++ RouteC41.G02.BLL/Repositries/EmployeeRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RouteC41.G02.BLL.Interfaces;
+using RouteC41.G02.DAL.Data;
+using RouteC41.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteC41.G02.BLL.Repositries
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeRepository(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        public int Add(Employee entity)
+        {
+            _context.Employees.Add(entity);
+            return _context.SaveChanges();
+
+        }
+        public int Update(Employee entity)
+        {
+            _context.Employees.Update(entity);
+            return _context.SaveChanges();
+
+        }
+        public int Delete(Employee entity)
+        {
+            _context.Employees.Remove(entity);
+            return _context.SaveChanges();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.AsNoTracking().ToList();
+        }
+
+        public Employee GetById(int id)
+        {
+            ///var Employee=_context.Employees.Local.Where(D=>D.Id==id).FirstOrDefault();
+            ///if (Employee==null)
+            ///	 Employee = _context.Employees.Where(D => D.Id == id).FirstOrDefault();
+            ///return Employee;
+
+            return _context.Employees.Find(id);
+
+        }
+    }
+}
